19d0a461aec81439a2134f28b30f084e3dfdcc5c
henriquebarros <henriquebarros@live.com>
2024-02-17 13:34:47 -0300

    configuring prism ORM

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,15 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+prisma.userOrg.create({
+  data: {
+    name: 'Pet Amigo',
+    email: 'henriquebarros@live.com',
+    whatsapp: '(85) 98888-8888',
+    address: 'Av. J, 100, PQ, CE',
+  },
+})
 
 export const app = fastify()
